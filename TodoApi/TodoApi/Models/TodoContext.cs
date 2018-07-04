afa1fe62dec2be32950801644a64b0b908869bac
Mirza <Constantin.Mirza@centric.eu>
2018-07-04 18:08:55 +0300

    Singleton created , get and put method

--- TodoApi/TodoApi/Models/TodoContext.cs
+++ TodoApi/TodoApi/Models/TodoContext.cs
@@ -15,4 +15,41 @@ namespace TodoApi.Models
 
         public DbSet<TodoItem> TodoItems { get; set; }
     }
+
+    public class Singleton
+    {
+        private static Singleton instance;
+        private System.Collections.Generic.List<TodoItem> ListItem;
+
+        private Singleton()
+        {
+            try
+            {
+                ListItem = new System.Collections.Generic.List<TodoItem>();
+            }
+            catch { }
+        
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if(instance==null)
+                    instance=new Singleton();
+                return instance;
+            }
+        }
+
+        public List<TodoItem> getAll()
+        {
+            return ListItem;
+        }
+
+        public void add(TodoItem item)
+        {
+            ListItem.Add(item);
+        }
+    }
 }
